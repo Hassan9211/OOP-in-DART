2f09e91229efe5715f26ab69077fee092670ca3a
Hassan Raza <99560525+Hassan9211@users.noreply.github.com>
2025-11-21 15:46:13 +0500

    Add files via upload

---
+++ null_value.dart
@@ -0,0 +1,23 @@
+void main(List<String> args) {
+  
+  int? age;
+  String? name;
+
+  print(age);
+  print(name);
+
+
+  if (name == null){
+    print('name is null');
+
+  }
+  name = "Hassan Ch";
+
+  age = 27;
+
+  print('Age is :$age');
+  print('Name is : $name');
+
+
+
+}
