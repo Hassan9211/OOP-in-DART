2e5d27fd2e08fd84e3173d852bcdbfb3b28b515a
Hassan Raza <99560525+Hassan9211@users.noreply.github.com>
2025-11-26 16:09:58 +0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,22 @@
+import 'person.dart';
+
+void main(List<String> args) {
+
+
+
+
+  Person person = Person("Hassan Raza", 27);
+
+
+  person.info_1();
+  person.setAge = 9;
+  person.setName = 'Abdullah Faiz';
+  print(person.name);
+  print(person.age);
+
+
+
+
+
+
+}
---
+++ person.dart
@@ -0,0 +1,38 @@
+
+
+
+class Person {
+  String _name;
+  int _age;
+
+  Person(this._name , this._age);
+
+  String get name  => _name;
+  int get age => _age;
+
+  set setAge(int age){
+    if (age > 0){
+      _age = age;
+
+    }
+
+  }
+  set setName(String name ){
+
+    _name = name;
+
+
+
+  }
+
+
+  void info_1(){
+    print('Name is : $_name ');
+    print('Age is : $_age');
+  }
+
+
+
+
+
+}
